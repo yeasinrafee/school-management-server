2c168c41c26f479e8d3401fe48805abd4aee9bbd
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-02-21 01:19:43 +0600

    Get all student and single student from DB

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -15,6 +15,35 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: "Students retrieved successfully!",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+      success: true,
+      message: "Student retrieved successfully!",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
+  getAllStudents,
+  getSingleStudent,
 };
--- src/app/modules/student/student.router.ts
+++ src/app/modules/student/student.router.ts
@@ -4,5 +4,7 @@ import { StudentControllers } from "./student.controller";
 const router = express.Router();
 
 router.post("/create-student", StudentControllers.createStudent);
+router.get("/", StudentControllers.getAllStudents);
+router.get("/:studentId", StudentControllers.getSingleStudent);
 
 export const StudentRoutes = router;
