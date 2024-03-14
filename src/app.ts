8e0193cf5cded86c704b3a504c482a4e0c183a44
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-03-14 14:42:38 +0600

    Creating not found route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { StudentRoutes } from "./app/modules/student/student.router";
 import { UserRoutes } from "./app/modules/user/user.route";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 const app: Application = express();
 
 // parsers
@@ -18,5 +19,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
