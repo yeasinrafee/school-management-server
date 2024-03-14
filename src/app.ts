b2c3bf0feee058849d955854533d83990b174bff
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-03-14 15:22:08 +0600

    Route indexing

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { StudentRoutes } from "./app/modules/student/student.router";
-import { UserRoutes } from "./app/modules/user/user.route";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
 const app: Application = express();
 
 // parsers
@@ -11,8 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Application Routers
-app.use("/api/v1/students", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
