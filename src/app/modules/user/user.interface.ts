0fb3fe3b2b8025184549b7d9d2862a50183e1aa6
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-03-14 12:27:54 +0600

    User schema and model

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -2,7 +2,7 @@ export type TUser = {
   id: string;
   password: string;
   needsPasswordChange: boolean;
-  role: "admin" | "student" | "faculty";
+  role: "admin" | "student" | "teacher";
   status: "in-progress" | "blocked";
   isDeleted: boolean;
 };
