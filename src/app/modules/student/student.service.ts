b2f014261a054fd46bd7f4d911f346cf920ac155
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-03-28 14:10:59 +0600

    Pagination added

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -20,7 +20,7 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
   });
 
   // Filtering
-  const excludeFields = ["searchTerm", "sort", "limit"];
+  const excludeFields = ["searchTerm", "sort", "limit", "page"];
   excludeFields.forEach((el) => delete queryObj[el]);
 
   const filterQuery = searchQuery.find(queryObj).populate("classDetails");
@@ -32,12 +32,21 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
 
   const sortQuery = filterQuery.sort(sort);
 
+  let page = 1;
   let limit = 1;
+  let skip = 0;
   if (query.limit) {
-    limit = query.limit;
+    limit = Number(query.limit);
   }
 
-  const limitQuery = await sortQuery.limit(limit);
+  if (query.page) {
+    page = Number(query.page);
+    skip = (page - 1) * limit;
+  }
+
+  const paginateQuery = sortQuery.skip(skip);
+
+  const limitQuery = await paginateQuery.limit(limit);
 
   return limitQuery;
 };
